82af17b93303e85c59916e7954fee3ca79d418eb
Dinesh Shah <shahdinesh909@gmail.com>
2019-04-10 08:29:02 +0545

    added validation in srudent list

--- app/Http/Controllers/StudentsController.php
+++ app/Http/Controllers/StudentsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Students;
 use Excel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class StudentsController extends Controller
 {
@@ -26,20 +27,36 @@ class StudentsController extends Controller
 
         if($data->count()){
             $students = [];
+            $errors = [];
             foreach ($data as $key => $value) {
-                if ($value->name == '')
-                    continue;
-                $students[] = [
+                $std = [
                     'name' => $value->name, 
                     'address' => $value->address,
-                    'phone' => (string)$value->phone,
+                    'phone' => $value->phone,
                     'dob' => $value->dob,
                 ];
+
+                $validation = Validator::make($std, [
+                    'name' => 'required', 
+                    'address' => 'required',
+                    'phone' => 'min:9|max:15|numeric',
+                    'dob' => 'max:10|regex:/\d{4}\/\d{1,2}\/\d{1,2}/',
+                ]);
+
+                if ($validation->fails())
+                    $errors[$std['name']] = $validation->messages()->all();
+                else
+                    $students[] = $std;
             }
 
             Students::insert($students);
 
-            redirect('/students');
+            // Send error message in view
+            echo "<pre>";
+            print_r($errors);
+            
+            // redirect to another page
+            // redirect('/students');
         }
     }
 }
